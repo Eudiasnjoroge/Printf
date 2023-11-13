4bc5e581871e546977a2b868887cf724b5e76889
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2023-11-13 12:20:23 -0800

    main.h

---
+++ printc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _printf_char - print characters
+ * @val: argument
+ * Return: 1
+ */
+int _printf_char(va_list val)
+{
+	char i;
+
+	i = var_arg(val, int);
+	_putchar(i);
+	return (1);
+}
