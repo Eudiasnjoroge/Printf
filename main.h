b47602cc0aec3298098ec17935f979e28064708a
Cheryl-Makunamu <makunamucheryl@gmail.com>
2023-11-13 12:09:59 -0800

    headerfile

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list list);
+int _putchar(char c);
+int print_str(va_list list);
+int _strlength(char *s);
+int _strlength(const char *s);
+int printper(void);
+int print_deci(va_list arg);
+int print_inte(va_list arg);
+
+
+
+
+
+#endif
